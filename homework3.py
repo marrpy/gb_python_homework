eace862726d7dd7449808291b9a8dcfef84a2344
marrpy <hakloi@mail.ru>
2021-09-23 15:15:29 +0300

    Урок 5. Работа с файлами

---
+++ homework3.py
@@ -0,0 +1,29 @@
+# Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов.
+# Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
+# Выполнить подсчет средней величины дохода сотрудников.
+
+file = open('salary.txt', 'r', encoding='utf-8')
+lines = file.readlines()
+
+num = 0
+def workers_statistics():
+    workers = [['\nСидоров ', '12000 \n']]
+    try:
+        with open('salary.txt', 'r+', encoding="utf-8") as file:
+            for i in range(len(workers)):
+                file.writelines(workers[i])
+            l = file.readlines()
+            poor = []
+            sum = 0
+            for i in range(len(l)):
+                salary = int((l[i].split())[1])
+                if salary < 20000:
+                    poor.append((l[i].split())[0])
+                sum += salary
+            print(f'Средняя величина дохода сотрудников равна {sum / len(workers) / 12:.2f}')
+            print(f'Меньше 20 тыс. рублей получает сотрудник: {", ".join(poor)}')
+    except FileNotFoundError:
+        return 'Файл не найден.'
+
+
+workers_statistics()
---
+++ homework7.py
@@ -0,0 +1,48 @@
+# Создать (не программно) текстовый файл, в котором каждая строка должна содержать данные о фирме:
+# название, форма собственности, выручка, издержки.
+# Пример строки файла: firm_1 ООО 10000 5000.
+# Необходимо построчно прочитать файл, вычислить прибыль каждой компании, а также среднюю прибыль.
+# Если фирма получила убытки, в расчет средней прибыли ее не включать.
+# Далее реализовать список. Он должен содержать словарь с фирмами и их прибылями, а также словарь со средней прибылью.
+# Если фирма получила убытки, также добавить ее в словарь (со значением убытков).
+# Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
+# Итоговый список сохранить в виде json-объекта в соответствующий файл.
+# Пример json-объекта:
+# [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
+
+import json
+
+
+def get_statistics():
+    try:
+        with open('file.txt', 'r+', encoding='utf-8') as file:
+            statistics = []
+            profit = {}
+            average_profit = {}
+            av = 0
+            prof = 0
+            i = 3
+            for line in file:
+                name, firm, earning, damage = line.split()
+                total = int(earning) - int(damage)
+                if total >= 0:
+                    prof = prof + total
+                else:
+                    i -= 1
+                profit[name] = total
+            statistics.append(profit)
+            if i != 0:
+                (av) = prof / i
+                average_profit['average_profit'] = round(av)
+                statistics.append(average_profit)
+            else:
+                print('Все компании работают в убыток')
+            print(statistics)
+        with open('file.json', 'a+', encoding='utf-8') as json_file:
+            json.dump(statistics, json_file)
+    except FileNotFoundError:
+        return 'Файл не найден.'
+
+
+get_statistics()
+
